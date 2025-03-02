13474c8640fd951c22d72138558d7c31b10b6202
hrid1 <hridoycse1@gmail.com>
2025-03-03 02:26:30 +0600

    Fully setup done with DB

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import authRouter from "./routes/auth.js";
-import 'dotenv/config'
+import "dotenv/config";
+import connectMongoDB from "./db/connectDB.js";
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello TweetX!");
 });
 
+console.log(process.env.MONGO_URI)
+
 app.use("/api/auth", authRouter);
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+  await connectMongoDB();
 });
+
